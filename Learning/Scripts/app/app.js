6c482dabccdc13409659b69b8482c31c310236c9
Annadurai <annadurai.net@live.com>
2015-09-29 22:05:41 +0530

    add angular js learning

---
+++ Learning/Scripts/app/app.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    var app = angular.module('store', []);
+
+    app.controller('strorecontroller', function () {
+        this.product = gem;
+        this.products = gems;
+    });
+
+    var gem = {
+        name: 'Dedecahedron',
+        price: 2.95,
+        description: 'Lorem Ipsum dummy contentLorem Ipsum dummy contentLorem Ipsum dummy content',
+        canPurchase: true,
+        soldOut:false,
+    }
+
+    var gems= [{
+        name: 'Dedecahedron',
+        price: 222.95,
+        description: 'Lorem Ipsum dummy contentLorem Ipsum dummy contentLorem Ipsum dummy content',
+        canPurchase: true,
+        soldOut: false,
+    },
+    {
+        name: 'Dumka',
+        price: 4.95,
+        description: 'Lorem Ipsum dummy contentLorem Ipsum dummy contentLorem Ipsum dummy content',
+        canPurchase: true,
+        soldOut: false,
+    },
+    {
+        name: 'Kumka',
+        price: 12.95,
+        description: 'Lorem Ipsum dummy contentLorem Ipsum dummy contentLorem Ipsum dummy content',
+        canPurchase: true,
+        soldOut: false,
+    },
+    {
+        name: 'Bilakki',
+        price: 8.95,
+        description: 'Lorem Ipsum dummy contentLorem Ipsum dummy contentLorem Ipsum dummy content',
+        canPurchase: true,
+        soldOut: false,
+    }]
+})();
+    
